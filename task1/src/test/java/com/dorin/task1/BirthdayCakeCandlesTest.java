242ac4ae912c7a1a0a2b185c6d6ac371c81082bb
doringribincea <>
2022-05-24 00:08:15 +0300

    Finish task1

--- task1/src/test/java/com/dorin/task1/BirthdayCakeCandlesTest.java
+++ task1/src/test/java/com/dorin/task1/BirthdayCakeCandlesTest.java
@@ -1,7 +1,7 @@
 package com.dorin.task1;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class BirthdayCakeCandlesTest {
 
@@ -9,27 +9,25 @@ public class BirthdayCakeCandlesTest {
     public void birthdayCakeCandlesSuccessTestCases1() {
         BirthdayCakeCandles birthdayCakeCandles = new BirthdayCakeCandles();
 
-        Assert.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
-                new int[]{4, 4, 3, 1}
-        ));
+        Assertions.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
+                new int[] { 4, 4, 3, 1 }));
+        ;
     }
 
     @Test
     public void birthdayCakeCandlesSuccessTestCases2() {
         BirthdayCakeCandles birthdayCakeCandles = new BirthdayCakeCandles();
 
-        Assert.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
-                new int[]{3, 2, 1, 3}
-        ));
+        Assertions.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
+                new int[] { 3, 2, 1, 3 }));
     }
 
     @Test
     public void birthdayCakeCandlesSuccessTestCases3() {
         BirthdayCakeCandles birthdayCakeCandles = new BirthdayCakeCandles();
 
-        Assert.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
-                new int[]{3, 2, 1, 3, 3, 3, 3, 7, 1, 7}
-        ));
+        Assertions.assertEquals(2, birthdayCakeCandles.birthdayCakeCandles(
+                new int[] { 3, 2, 1, 3, 3, 3, 3, 7, 1, 7 }));
     }
 
     @Test
@@ -38,22 +36,20 @@ public class BirthdayCakeCandlesTest {
 
         try {
             birthdayCakeCandles.birthdayCakeCandles(
-                    new int[]{}
-            );
+                    new int[] {});
 
-            Assert.fail();
+            Assertions.fail();
         } catch (Exception e) {
-            Assert.assertTrue(e instanceof IllegalArgumentException);
+            Assertions.assertTrue(e instanceof IllegalArgumentException);
         }
 
         try {
             birthdayCakeCandles.birthdayCakeCandles(
-                    null
-            );
+                    null);
 
-            Assert.fail();
+            Assertions.fail();
         } catch (Exception e) {
-            Assert.assertTrue(e instanceof IllegalArgumentException);
+            Assertions.assertTrue(e instanceof IllegalArgumentException);
         }
     }
 
